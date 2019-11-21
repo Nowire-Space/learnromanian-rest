b65ec305a2f49250760e40a7b769c020b2d4fb5f
nicolae.nichiforeac <nicolae.nichiforeac@crystal-system.eu>
2019-11-21 13:08:18 +0200

    Initial commit

---
+++ src/main/java/ro/ugal/learnromanian/LearnromanianRestApplication.java
@@ -0,0 +1,13 @@
+package ro.ugal.learnromanian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnromanianRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnromanianRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/ugal/learnromanian/LearnromanianRestApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.ugal.learnromanian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnromanianRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
